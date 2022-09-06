48b81483bd4465dbe40e2773e09f93c24cf36e27
TeunMos <teun.mos@gmail.com>
2022-09-06 13:33:00 +0200

    Added Hello world

---
+++ react-test/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Welcome from './Components/Test.jsx'
+
+function App() {
+  return (
+    <div className="App">
+      <Welcome name='world'></Welcome>
+    </div>
+  );
+}
+
+
+
+export default App;
